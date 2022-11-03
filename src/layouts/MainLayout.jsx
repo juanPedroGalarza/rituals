d7792c2bd9c1717809d23fc5ef37c04523ea9f47
juanPedroGalarza <juanpg232@gmail.com>
2022-11-03 18:02:26 -0300

    hidden footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,16 @@
 import Footer from "../components/Footer"
 import Nav from "../components/Nav"
-export default function MainLayout(props){
+export default function MainLayout(props) {
+    const list = [
+        {to:"/",title:"Home"},
+        {to:"/play",title:"Play"},
+        {to:"/help",title:"Help"},
+    ]
     return (
         <>
-            <Nav />
+            <Nav list={list} />
             {props.children}
-            <Footer/>
+            {/* <Footer list={list} /> */}
         </>
     )
 }
