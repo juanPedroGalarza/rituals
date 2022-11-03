ae5ed6d90658278b2f944ea02144f6f26e62e42b
juanPedroGalarza <juanpg232@gmail.com>
2022-11-03 18:02:55 -0300

    nav update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
 import "../styles/Nav.css"
-export default function Nav() {
+export default function Nav(props) {
+    const [drop, setDrop] = useState(true)
+    const printItem = (item) => {
+        return (
+            <Link to={item.to}>{item.title}</Link>
+        )
+    }
     return (
         <nav className="nav-container">
-            <h1 className="nav-title">RITUALS</h1>
-            <div className="nav-list"></div>
+            <h1 className="nav-title" onClick={() => setDrop(!drop)}>
+            {drop ? "▼" : "▲"} RITUALS</h1>
+            {drop ? <div className="nav-list">
+                {props.list?.map(printItem)}</div> : null}
         </nav>
     )
 }
