f22ad1eba3454970a3466009f6004d7214c5dde8
juanPedroGalarza <juanpg232@gmail.com>
2022-11-03 16:57:33 -0300

    Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from "./layouts/MainLayout"
+import Home from './pages/Home';
+import Play from './pages/Play';
 
 function App() {
   return (
     <BrowserRouter>
-      <MainLayout/>
+      <MainLayout>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/play' element={<Play/>} />
+          <Route path='/*' element={<Home/>} />
+        </Routes>
+      </MainLayout>
     </BrowserRouter>
   );
 }
