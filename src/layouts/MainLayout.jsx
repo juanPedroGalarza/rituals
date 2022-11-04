b2451d4c922777a22a49c7e3934c1f01219779cb
juanPedroGalarza <juanpg232@gmail.com>
2022-11-04 10:42:07 -0300

    comment import

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,4 +1,4 @@
-import Footer from "../components/Footer"
+// import Footer from "../components/Footer"
 import Nav from "../components/Nav"
 export default function MainLayout(props) {
     const list = [
