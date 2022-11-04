df5ac6f8d17e5d0cf9c1a348888777f3a9515991
juanPedroGalarza <juanpg232@gmail.com>
2022-11-04 15:07:18 -0300

    WriteTExt updated

--- src/components/WriteText.jsx
+++ src/components/WriteText.jsx
@@ -1,24 +1,27 @@
 import { useCallback, useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import { startWriting, stopWriting } from "../features/game/gameSlice"
 
 export default function WriteText(props) {
     const [count, setCount] = useState(1)
-
-    const changeCount = (interval,count,length) => {
+    const dispatch = useDispatch()
+    const text = props.children
+    const changeCount = (interval, count, length) => {
         if (count < length) {
-        let intervalId = setInterval(() => {
-            setCount(c => c + 1)
-            return clearInterval(intervalId)
-        }, interval)
+            let intervalId = setInterval(() => {
+                setCount(c => c + 1)
+                dispatch(stopWriting())
+                return clearInterval(intervalId)
+            }, interval)
+        } else {
+            dispatch(startWriting())
         }
     }
-    const memoizedChangeCount = useCallback(() => {
-        return changeCount(props.interval, count, props.children.length)
-        },
-        [count, props.children.length, props.interval])
+    const memoizedChangeCount = useCallback(changeCount,[dispatch])
     useEffect(() => {
-        return () => memoizedChangeCount()
-    }, [memoizedChangeCount])
+        return () => memoizedChangeCount(props.interval,count,text?.length)
+    }, [memoizedChangeCount,props.interval,count,text?.length])
     return (
-        <p>{props.children.slice(0, count)}</p>
+        <p>{text?.slice(0, count)}</p>
     )
 }
