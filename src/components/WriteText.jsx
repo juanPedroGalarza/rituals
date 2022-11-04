00b1de88a3aec5e13a844ff318e2b3dc18209a4b
juanPedroGalarza <juanpg232@gmail.com>
2022-11-04 10:36:42 -0300

    optimize WriteTExt

--- src/components/WriteText.jsx
+++ src/components/WriteText.jsx
@@ -1,19 +1,24 @@
-import { useState, useEffect } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 export default function WriteText(props) {
     const [count, setCount] = useState(1)
-    const [intervalId, setIntervalId] = useState()
 
-    useEffect(() => {
-        if (count < props.children.length) {  
-            let id = setInterval(()=>setCount(c=>c+1), props.interval)
-            setIntervalId(id)
-            return () => clearInterval(intervalId)
+    const changeCount = (interval,count,length) => {
+        if (count < length) {
+        let intervalId = setInterval(() => {
+            setCount(c => c + 1)
+            return clearInterval(intervalId)
+        }, interval)
         }
-    },[])
+    }
+    const memoizedChangeCount = useCallback(() => {
+        return changeCount(props.interval, count, props.children.length)
+        },
+        [count, props.children.length, props.interval])
+    useEffect(() => {
+        return () => memoizedChangeCount()
+    }, [memoizedChangeCount])
     return (
-        <>
-            <p>{props.children.slice(0, count)}</p>
-        </>
+        <p>{props.children.slice(0, count)}</p>
     )
 }
