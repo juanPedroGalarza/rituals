5d459a4dc89c3c1ab4e3c02a7c822c68d9cd6cbf
juanPedroGalarza <juanpg232@gmail.com>
2022-11-05 20:51:13 -0300

    all chapters

--- src/assets/text/caps.js
+++ src/assets/text/caps.js
@@ -1,5 +1,28 @@
 import cap0 from "./caps/cap0"
+import cap1 from "./caps/cap0"
+import cap2 from "./caps/cap0"
+import cap3 from "./caps/cap0"
+import cap4 from "./caps/cap0"
+import cap5 from "./caps/cap0"
+import cap6 from "./caps/cap0"
+import cap7 from "./caps/cap0"
+import cap8 from "./caps/cap0"
+import cap9 from "./caps/cap0"
+import cap10 from "./caps/cap0"
+import cap11 from "./caps/cap0"
+
 const caps = [
-    cap0
+    cap0,
+    cap1,
+    cap2,
+    cap3,
+    cap4,
+    cap5,
+    cap6,
+    cap7,
+    cap8,
+    cap9,
+    cap10,
+    cap11,
 ]
 export default caps
