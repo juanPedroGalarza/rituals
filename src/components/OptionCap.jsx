0e94d6d638247ef2ec5689150cfdfd7e2f056831
juanPedroGalarza <juanpg232@gmail.com>
2022-11-06 00:35:47 -0300

    play update

--- src/components/OptionCap.jsx
+++ src/components/OptionCap.jsx
@@ -1,10 +1,31 @@
 import { useRef } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { selectOpt } from "../features/game/gameSlice"
 
 export default function OptionCap({option}) {
     const dispatch = useDispatch()
     const inputEl = useRef(null)
+    const {isMuted} = useSelector(state=>state.game)
+
+    if (option.condition) {
+        switch (option.condition) {
+            case "MUTED":
+                if(isMuted==="NO") return null
+                break;
+            case "NO_MUTED":
+                if(isMuted==="YES") return null
+                break;
+            case "EXCLUSIVE_MUTED":
+                if(isMuted!=="YES") return null
+                break;
+            case "EXCLUSIVE_NO_MUTED":
+                if(isMuted!=="NO") return null
+                break;
+            default:
+                break;
+        }
+    }
+
     return (
         <label className={`panel-option ${inputEl.current?.checked ? "active" : ""}`}
             onChange={() => dispatch(selectOpt(option))}>
--- src/components/PanelText.jsx
+++ src/components/PanelText.jsx
@@ -1,30 +1,80 @@
 import WriteText from "./WriteText"
-import {useSelector} from "react-redux"
+import {useDispatch, useSelector} from "react-redux"
 import "../styles/Play.css"
 import OptionCap from "./OptionCap"
+import { nextCap, emptyText, setFinal, reset, setMuted } from "../features/game/gameSlice"
+import { useNavigate } from "react-router-dom"
 
 export default function PanelText() {
-    const { selected, isWrited, optSelected } = useSelector(state => state.game)
-    const nextCap = () => {
-        console.log(optSelected)
+    const { selected, isWrited, optSelected, isFinal } = useSelector(state => state.game)
+    const dispatch = useDispatch()
+    const finalText = "¿Quieres volver a jugar?"
+    const navigate = useNavigate()
+    const toDoSelected = (action) => {
+        switch (action) {
+            case "IS_MUTED":
+                dispatch(setMuted("YES"))
+                break;
+            case "NO_MUTED":
+                dispatch(setMuted("NO"))
+                break;
+            default:
+                break;
+        }
     }
+
+    const next = () => {
+        dispatch(emptyText())
+        setTimeout(() => {
+            if (optSelected.cap) {
+                optSelected.action && toDoSelected(optSelected.action)
+                dispatch(nextCap(optSelected))
+            } else {
+                dispatch(setFinal())
+            }
+        },500)
+    }
+    const playAgain = () => {
+        dispatch(emptyText())
+        setTimeout(() => {
+            dispatch(reset())
+        },500)
+    }
+
     return (
         <div className="panel-container">
             <div className="panel-text">
-                <WriteText interval={50}>{selected.text}</WriteText>
+                {selected ?
+                    <WriteText interval={50}>{selected.text}</WriteText> : null}
+                {isFinal ? <WriteText interval={50}>{finalText}</WriteText> : null}
             </div>
-            {isWrited ?
+            {isWrited && selected ?
                 <form className="panel-form">
                     <div className="panel-options">
-                        {selected.options?.map((o) => <OptionCap key={o.selection} option={o} />)}
+                        {selected.options?.map((o, i) => <OptionCap key={i} option={o} />)}
                     </div>
                     <button
                         type="button"
                         className={`panel-submit ${optSelected ? "" : "chose"}`}
-                        onClick={optSelected ? nextCap : null}>
+                        onClick={optSelected ? next : null}>
                         {optSelected ? "Continuar" : "Elige"}</button>
                 </form>
                 : null}
+            {isWrited && isFinal ?
+                <>
+                    <button
+                        type="button"
+                        className="panel-submit"
+                        onClick={playAgain}
+                    >SI</button>
+                    <button
+                        type="button"
+                        className="panel-submit"
+                        onClick={()=>navigate("/")}
+                    >NO</button>
+                </>
+                : null
+            }
         </div>
     )
 }
