7bb09919cf2ba1f791b05493c9ad1df7f5fbd461
juanPedroGalarza <juanpg232@gmail.com>
2023-02-09 16:42:08 -0300

    proyect converted to vite typescript

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {store} from './features/store'
 import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
-);
-
-reportWebVitals();
+  </React.StrictMode>,
+)
