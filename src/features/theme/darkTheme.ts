521aefada5cb067fa752c9606e21ef40bfc72c79
juanPedroGalarza <juanpg232@gmail.com>
2023-02-12 19:16:14 -0300

    update palette on themes

--- src/features/theme/darkTheme.ts
+++ src/features/theme/darkTheme.ts
@@ -1,13 +1,18 @@
-import { blue, grey } from '@mui/material/colors';
+import { lightBlue, grey, red } from '@mui/material/colors';
 import { createTheme, Theme } from '@mui/material/styles';
 
 const theme: Theme = createTheme({
   palette: {
     mode: "dark",
     primary: {
-      main: blue[500],
-      dark: "#155fa0",
-      light: "#4b9fea"
+      main: lightBlue[700],
+      dark: "#015f92",
+      light: "#349fda"
+    },
+    secondary: {
+      main: red[700],
+      dark: "#932020",
+      light: "#db5858"
     },
     background: {
       paper: grey[900]
--- src/features/theme/lightTheme.ts
+++ src/features/theme/lightTheme.ts
@@ -1,12 +1,17 @@
-import { blue, blueGrey } from '@mui/material/colors';
+import { lightBlue, blueGrey, red } from '@mui/material/colors';
 import { createTheme, Theme } from '@mui/material/styles';
 
 const theme: Theme = createTheme({
   palette: {
     primary: {
-      main: blue[500],
-      dark: "#0e4686",
-      light: "#4383cc"
+      main: lightBlue[900],
+      dark: "#003c6c",
+      light: "#3378af"
+    },
+    secondary: {
+      main: red[900],
+      dark: "#801313",
+      light: "#c54949"
     },
     background: {
       paper: blueGrey[100]
