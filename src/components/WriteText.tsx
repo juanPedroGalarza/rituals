6d7b8125e7c21a9c5c269c2dceb017681b1935e9
juanPedroGalarza <juanpg232@gmail.com>
2023-02-14 22:39:27 -0300

    optimize WriteText & converted to typescript

---
+++ src/components/WriteText.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { startWriting, stopWriting } from "../features/game/gameSlice";
+
+interface MyProps {
+  children: string,
+  interval: number
+}
+
+export default function WriteText(props:MyProps) {
+  const [count, setCount] = useState<number>(1);
+  const [intervalId, setIntervalId] = useState<undefined | NodeJS.Timer>();
+  const dispatch = useDispatch();
+  const text = props.children;
+
+  useEffect(():VoidFunction => {
+    return ():void => clearInterval(intervalId);
+  }, [intervalId]);
+
+
+  useEffect(():void => {
+    setCount(0);
+    text && dispatch(startWriting());
+  }, [text, dispatch]);
+
+  useEffect(():void => {
+    if (count >= text.length) {
+      dispatch(stopWriting());
+    } else if (text.length && count < text.length) {
+      dispatch(startWriting());
+      let id = setInterval(() => {
+        setCount(c => c + 1);
+      }, props.interval);
+      setIntervalId(id);
+    };
+  }, [props.interval, count, dispatch, text.length]);
+  return (
+    <p>{text?.slice(0, count)}</p>
+  );
+};
