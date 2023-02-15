6ef61f21344a91b6b91382a4567a038f66867f74
juanPedroGalarza <juanpg232@gmail.com>
2023-02-15 17:13:29 -0300

    delete files unused

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import "../styles/Footer.css"
-export default function Footer() {
-    return (
-        <footer className="footer-container">
-            <p className="footer-title">Rituals</p>
-        </footer>
-    )
-}
