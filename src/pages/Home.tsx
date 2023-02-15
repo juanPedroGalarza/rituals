001d1aa94a775249ceb9022bfed553dcca50da4c
juanPedroGalarza <juanpg232@gmail.com>
2023-02-15 17:07:42 -0300

    Home styles and mui components, delete styles folder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,32 @@
 import WriteText from "../components/WriteText";
 import text from "../assets/text/home";
-import "../styles/Home.css"
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import MainStyled from "../components/styled/MainStyled";
+import { Typography, Box, Button } from "@mui/material";
 
 export default function Home() {
   return (
     <MainStyled>
-      <p>{text.title}</p>
-      <div className="home-description">
+      <Typography variant="h3">{text.title}</Typography>
+      <Box sx={{
+        display: "flex",
+        justifyContent: "flex-start",
+        width: "100%",
+        maxWidth: 600,
+      }}>
         <WriteText interval={100}>{text.description}</WriteText>
-      </div>
-      <Link to="/jugar">{"JUGAR :)"}</Link>
+      </Box>
+      <Button
+        variant="contained"
+        color="primary"
+        component={Link}
+        to="/jugar"
+        endIcon={<Typography
+          sx={{ transform: "rotate(90deg)" }}>{":)"}</Typography>}
+      >
+        <Typography sx={{ marginRight: ".5rem" }}
+        >JUGAR</Typography>
+      </Button>
     </MainStyled>
   );
 };
