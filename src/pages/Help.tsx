2f99bc0ba5dd1fb4072e71a8783fa908c371d302
juanPedroGalarza <juanpg232@gmail.com>
2023-02-17 12:25:00 -0300

    Help Page interactive

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -1,15 +1,33 @@
 import WriteText from "../components/WriteText";
 import MainStyled from "../components/styled/MainStyled";
+import { useState } from "react";
+import { Icon, IconButton, Box } from "@mui/material";
 
 export default function Help(){
+  const [open, setOpen] = useState<boolean>(false)
+  const [srtartAnimation, setSrtartAnimation] = useState<boolean>(false)
   return (
     <MainStyled>
-      <div style={{ width: "100%", maxWidth: 400 }}>
+      {open ?
+      <Box sx={{ width: "100%", maxWidth: 400 }}>
         <WriteText interval={30}>
           Cada vez que avances se te presentaran varias opciones
           La historia cambiara por cada eleccion que tomes.
           Asi que ten mucho cuidado con lo que eliges</WriteText>
-      </div>
+      </Box>
+        :
+        <IconButton onClick={() => setSrtartAnimation(true)}
+          sx={{
+            fontSize:"4rem",
+            transition: "all 2s ease-in",
+            transform: srtartAnimation?"scale(5)":null,
+            opacity: srtartAnimation ? "0%" : "100%"
+          }}
+          edge="start"
+          onTransitionEndCapture={() => setOpen(true)}
+          color="warning">
+          <Icon fontSize="inherit">question_mark</Icon>
+        </IconButton>}
     </MainStyled>
   );
 };
